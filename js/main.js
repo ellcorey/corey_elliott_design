6c7889a89d9f904a7933728849a7b517f3707d24
ellcorey <corey.elliott@mac.com>
2015-11-16 17:11:31 -0600

    updated index page with a first breakpoint @ 720px and styled accordingly. still need to add aditional breakpoints to smooth out the upsizing.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 console.log('test')
 
+// FOR MENU TOGGLE
+
 $(document).ready(function() {
   var menuToggle = $('#js-mobile-menu').unbind();
   $('#js-navigation-menu').removeClass("show");
@@ -14,6 +16,28 @@ $(document).ready(function() {
   });
 });
 
+// FOR HAMBURGER ICON ANIMATION
+(function() {
+
+  "use strict";
+
+  var toggles = document.querySelectorAll(".c-hamburger");
+
+  for (var i = toggles.length - 1; i >= 0; i--) {
+    var toggle = toggles[i];
+    toggleHandler(toggle);
+  };
+
+  function toggleHandler(toggle) {
+    toggle.addEventListener( "click", function(e) {
+      e.preventDefault();
+      (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+    });
+  }
+
+})();
+
+// FOR SPLASH SCREEN
 $( document ).ready(function() {
   if($(".splash").is(":visible"))
   {
