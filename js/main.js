384aa22916a1e2ab4bd641d4f34b30d25ac24b3b
ellcorey <corey.elliott@mac.com>
2015-11-15 22:34:07 -0600

    took index html and styling from single page project and added it. splash page functions as expected now.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,54 @@
 console.log('test')
 
 $(document).ready(function() {
-  var menuToggle = $('#js-centered-navigation-mobile-menu').unbind();
-  $('#js-centered-navigation-menu').removeClass("show");
-  
+  var menuToggle = $('#js-mobile-menu').unbind();
+  $('#js-navigation-menu').removeClass("show");
+
   menuToggle.on('click', function(e) {
     e.preventDefault();
-    $('#js-centered-navigation-menu').slideToggle(function(){
-      if($('#js-centered-navigation-menu').is(':hidden')) {
-        $('#js-centered-navigation-menu').removeAttr('style');
+    $('#js-navigation-menu').slideToggle(function(){
+      if($('#js-navigation-menu').is(':hidden')) {
+        $('#js-navigation-menu').removeAttr('style');
       }
     });
   });
 });
 
+$( document ).ready(function() {
+  if($(".splash").is(":visible"))
+  {
+    $(".wrapper").css({"opacity":"0"});
+  }
+  $(".splash-arrow").click(function()
+  {
+    $(".splash").slideUp("800", function() {
+        $(".wrapper").delay(100).animate({"opacity":"1.0"},800);
+     });
+  });
+});
 
+/*
+$(window).scroll(function() {
+  var windTop = $(window).scrollTop();
+  var splashHeight = $(".splash").height();
+  
+  if(windTop>(100)){
+     $(window).off("scroll");
+      $(".splash").slideUp("800", function() {
+       $("html, body").animate({"scrollTop":"0px"},100);
+     });
+     $(".wrapper").animate({"opacity":"1.0"},800);
+  } 
+  else {
+  
+  }  
+});
+*/
+
+$(window).scroll(function() {
+      $(window).off("scroll");
+    $(".splash").slideUp("800", function() {
+    $("html, body").animate({"scrollTop":"0px"},100);
+    $(".wrapper").delay(100).animate({"opacity":"1.0"},800);
+ });
+ });
